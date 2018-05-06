fab0957074a53128ce6cc80f184204ee706174aa
Jaime McCann <bimccann@ucsc.edu>
2018-05-05 18:20:01 -0700

    Changed the way assets will be loaded, in preparation for temp asset development. It's become clear I won't be able to test my code without temp assets, so I'm going to start making some.

---
+++ js/game/assetPath.js
@@ -0,0 +1,4 @@
+/*  game/assetPath
+    Are we using the real assets or the temp assets? */
+let ASSETS_READY = false;
+define((ASSETS_READY ? 'assets' : 'assets/temp'));
--- js/game/states/setup.js
+++ js/game/states/setup.js
@@ -4,8 +4,7 @@
     Bryan, you don't have to worry about making the audiosprite.
     I found a program that will handle it if you just upload the audio files.
     The reason I want us to use an audiosprite is for coding convenience. */
-let HAVE_ASSETS = false;
-define(['game/game', 'util/functional'], function (game, F) {
+define(['game/assetPath', 'game/game', 'util/functional'], function (assetPath, game, F) {
     game.state.add('setup', {
         init: function () {
             let args = F.arrayOf.apply(F, arguments);
@@ -16,22 +15,20 @@ define(['game/game', 'util/functional'], function (game, F) {
             Phaser.Canvas.setSmoothingEnabled(game.context, false);
             game.time.advancedTiming = true;
             game.stage.backgroundColor = '#000000';
-            if (HAVE_ASSETS) {
-                game.load.atlas('atlas', 'assets/graphics/atlas.png', 'assets/graphics/atlas.json',
-                    Phaser.Loader.TEXTURE_ATLAS_JSON_HASH);
-                game.load.image('tileset', 'assets/graphics/tileset.png');
-                game.load.audioSprite('audiosprite',
-                    ['assets/audio/audiosprite.mp3',
-                     'assets/audio/audiosprite.ogg',
-                     'assets/audio/audiosprite.m4a'],
-                    'assets/audio/audiosprite.json');
-            }
+            game.load.atlas('atlas',
+                assetPath + '/graphics/atlas.png',
+                assetPath + '/graphics/atlas.json',
+                Phaser.Loader.TEXTURE_ATLAS_JSON_HASH);
+            game.load.image('tileset', assetPath + '/graphics/tileset.png');
+            game.load.audioSprite('audiosprite',
+                [assetPath + '/audio/audiosprite.mp3',
+                 assetPath + '/audio/audiosprite.ogg',
+                 assetPath + '/audio/audiosprite.m4a'],
+                assetPath + '/audio/audiosprite.json');
         },
         create: function () {
-            if (HAVE_ASSETS) {
-                game.audiosprite = game.make.audioSprite('audiosprite');
-                game.stage.addChild(game.audiosprite);
-            }
+            game.audiosprite = game.make.audioSprite('audiosprite');
+            game.stage.addChild(game.audiosprite);
         },
         update: function () {
             F.curry(game.state.start, this.nextState,
