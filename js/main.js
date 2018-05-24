816717fd6a57e582aa4ec406dd6026ffa1d27c5d
Jaime McCann <bimccann@ucsc.edu>
2018-05-23 18:21:25 -0700

    Added third level

--- js/main.js
+++ js/main.js
@@ -8,14 +8,15 @@ requirejs(['game/states/setup',
             'game/states/skeletons/player',
             'game/states/skeletons/playertest',
             'game/states/skeletons/space_level',
+            'game/states/skeletons/side_to_side',
             'game/LevelOrder'],
-function (setup, skelcomp, groups, player, playertest, spaceLevel, LevelOrder) {
+function (setup, skelcomp, groups, player, playertest, spaceLevel, sideToSide, LevelOrder) {
     /*  Runs the preloader and then transitions to the state skeleton compositor.
         The SSC will then make a state out of:
         * the groups skeleton (which makes sure the state has a 'groups' property)
         * the player skeleton (which adds the player character)
         * the playertest skeleton (which adds the silly little test room) */
-    LevelOrder.order = [playertest, spaceLevel];
+    LevelOrder.order = [playertest, spaceLevel, sideToSide];
     LevelOrder.current = 0;
     setup('skelcomp', groups, player, playertest);
 });
