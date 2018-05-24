35dd861acee7e2f2614e1dd339a395b5b0f13034
Jaime McCann <bimccann@ucsc.edu>
2018-05-23 18:02:36 -0700

    Fixed merge conflict more properly

--- js/game/states/skeletons/space_level.js
+++ js/game/states/skeletons/space_level.js
@@ -26,10 +26,6 @@ define(['game/keyDown', 'util/functional', 'util/vectorMath'], function (keyDown
 	},
     create: function () {
 		this.game.stage.backgroundColor = '#034b59';
-		let background = this.game.add.image(0,0,'atlas','moon');
-		background.anchor.setTo(0,1);
-		background.width = this.game.width;
-		background.height = -1*(this.game.height/5);
 		
 		this.timer = this.game.time.create(false);
 		this.timer.loop(1300, function () {
@@ -63,7 +59,6 @@ define(['game/keyDown', 'util/functional', 'util/vectorMath'], function (keyDown
         this.background.body.immovable=true;
         this.background.width = this.game.width;
         this.background.height = -1*(this.game.height/5);
-        this.game.physics.arcade.enable(this.background);
         this.world.sendToBack(this.groups.solids);
         this.world.sendToBack(this.triangularDude);
         for(let i = -1; i<10; i++)
