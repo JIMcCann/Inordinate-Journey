54f8b0abaf5cba9d6b609094c530eef1f49f2c88
Jaime McCann <bimccann@ucsc.edu>
2018-05-29 17:13:27 -0700

    Added exploding fireballs

--- js/game/states/skeletons/playertest.js
+++ js/game/states/skeletons/playertest.js
@@ -68,11 +68,45 @@ function (keyDown, portal, F, VM) {return {
         let startplatform = this.spawnPlatform('platform-1',
             this.player.x - exampleplat.width/2,
             this.player.y + this.player.height/2);
-        this.groups.hazards = this.add.group();
-        this.groups.hazards.enableBody = true;
         this.portalTimeout = 1000;
     },
+    spawnFireball: function () {
+        let fball = this.groups.hazards.create(Math.random()*this.game.width, 570, 'atlas', 'fireball');
+        fball.anchor.setTo(0.5);
+        fball.scale.setTo(Math.random() + 1.5);
+        fball.body.velocity.y = -Math.random()*300 - 300;
+        fball.body.velocity.x = Math.random()*250 - 125;
+        fball.body.gravity.y = 300;
+        fball.fliptimeout = 15;
+        let STATE = this;
+        fball.update = function () {
+            this.angle = VM.angle(this.body.velocity) + 90;
+            fball.fliptimeout--;
+            if (fball.fliptimeout <= 0) {
+                fball.fliptimeout = 15;
+                fball.scale.x *= -1;
+            }
+            if (Math.random() < 0.005) {
+                if (this.scale.y > 1) {
+                    let parts = Math.ceil(Math.random()*5) + 1;
+                    for (let i = 0; i < parts; i++) {
+                        let part = STATE.spawnFireball();
+                        part.x = this.x;
+                        part.y = this.y;
+                        part.scale.setTo(this.scale.y/Math.sqrt(parts));
+                        part.body.velocity.x += this.body.velocity.x;
+                        part.body.velocity.x /= 2;
+                        part.body.velocity.y += this.body.velocity.y;
+                        part.body.velocity.y /= 2;
+                    }
+                    this.destroy();
+                }
+            }
+        };
+        return fball;
+    },
     update: function () {
+        if (Math.random() < 0.01) this.spawnFireball();
 
 		//this.groups.solids.forEach(function(platform){platform.body.y += 1;});
 		// Using velocity for this to fix player bounce glitch
