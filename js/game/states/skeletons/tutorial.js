bd26388750e750a6b718734c1e4140dadef854a2
BryanArvizu <35119502+BryanArvizu@users.noreply.github.com>
2018-06-01 19:42:45 -0700

    Worked on tutorial!

--- js/game/states/skeletons/tutorial.js
+++ js/game/states/skeletons/tutorial.js
@@ -1,21 +1,54 @@
 /*  game/states/skeletons/tutorial
     Tutorial level with goofy geometry */
-define(['game/states/skeletons/portal'], function (portal) {return {
+define(['game/states/skeletons/portal', 'game/states/skeletons/ticktimer'], function (portal, ticktimer) {return {
+	spawnPlatform: function (name, x, y) {
+        let plat = this.groups.solids.create(x, y, 'atlas', name);
+        plat.scale.setTo(2);
+        plat.body.immovable = true;
+        plat.body.velocity.y = 60;
+        plat.body.checkCollision.down = false;
+        plat.body.checkCollision.left = false;
+        plat.body.checkCollision.right = false;
+        return plat;
+    },
+	createText: function (x, y, text, style){
+		let t = new Text(this.game, x, y, text, style);
+		return t;
+	},
     preload: function () {
-        this.load.tilemap('testmap', 'assets/maps/testmap.json', null,
-            Phaser.Tilemap.TILED_JSON);
         this.load.image('arrowkeys', 'assets/graphics/arrowkeys.png');
-        this.load.image('tileset', 'assets/graphics/tileset.png');
     },
     create: function () {
+		// Set Background Color just in case
+		this.game.stage.backgroundColor = '#034b59';
+		// Start physics
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
-        this.tilemap = this.add.tilemap('testmap');
-        this.tilemap.addTilesetImage('tileset', 'tileset');
-        this.tilemap.setCollisionBetween(1, 100);
-        this.solidLayer = this.tilemap.createLayer('solid');
-        //this.groups.solids = this.add.group();
-        //this.groups.solids.add(this.solidLayer);
-        this.physics.enable(this.solidLayer);
+		this.player.body.collideWorldBounds=true;
+
+		
+		this.groups.solids = this.add.group();
+		this.groups.solids.enableBody = true;
+		
+		this.ground = this.groups.solids.create(0, this.game.height-100, 'atlas', 'moon');
+		this.ground.width = this.game.width;
+		this.ground.height = 100;
+		this.ground.body.immovable = true;
+		
+		this.game.world.sendToBack(this.groups.solids);
+		this.game.world.bringToTop(this.ground);
+		
+		// timer to spawn a platform on interval
+		this.addSkel(ticktimer);
+		
+		// Timer event to spawn platforms
+		this.addTicktimerEvent(100, function (ticks) {
+		    let p = this.spawnPlatform(
+		        'platform-moon-' + (Math.floor(Math.random()*3) + 1),
+		        Math.random()*(this.game.width-100), 0);
+		    return 1;
+		});
+        
+        
         this.arrowkeysImage = this.add.image(100, 200, 'arrowkeys');
         this.arrowkeysImage.scale.setTo(0.75);
         this.addSkel(portal);
