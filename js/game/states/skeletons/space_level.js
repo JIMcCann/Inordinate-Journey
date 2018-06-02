100fd1dea458f31e498190eb3f8fd4ee1b2c7fb2
BryanArvizu <35119502+BryanArvizu@users.noreply.github.com>
2018-06-01 18:31:16 -0700

    Commented and cleaned up space_level.js

--- js/game/states/skeletons/space_level.js
+++ js/game/states/skeletons/space_level.js
@@ -2,24 +2,22 @@
     A dumb test map with smiley geometry shapes */
 define(['game/keyDown', 'game/states/skeletons/portal', 'game/states/skeletons/spacebg',
     'game/states/skeletons/triangle', 'game/states/skeletons/ticktimer',
-    'game/states/skeletons/flashingText',
     'util/functional', 'util/vectorMath'],
-function (keyDown, portal, spacebg, triangle, ticktimer, flashingText, F, VM) {return {
-    preload: function () {
-		// Hi!
-		// wEELLLLl heLLO theR
-    },
-    spawnPlatform: function (name, x, y) {
+function (keyDown, portal, spacebg, triangle, ticktimer, F, VM) {return {
+	// Function that spawns a platform
+	spawnPlatform: function (name, x, y) {
+		// Create Solid Platform
         let plat = this.groups.solids.create(x, y, 'atlas', name);
         plat.scale.setTo(2);
         plat.body.immovable = true;
-        plat.body.velocity.y = -75;
-        plat.body.checkCollision.up = false;
-        plat.body.checkCollision.left = false;
+        plat.body.velocity.y = -75; // Set velocity to negative so it moves upwards
+        plat.body.checkCollision.up = false;	// do not check collision on
+        plat.body.checkCollision.left = false;	// all sides but the bottom
         plat.body.checkCollision.right = false;
         return plat;
     },
-	entityWrap: function (entity) { // Function to an entity wrap to the other side of the screen
+	// Function to an entity wrap to the other side of the screen
+	entityWrap: function (entity) { 
 		if(entity.x > this.game.width + Math.abs(entity.width)){ // if entity leaves right side completely,
 				entity.x = 0 - Math.abs(entity.width); // then place the entity barely off-screen on the left
 			}
@@ -29,15 +27,21 @@ function (keyDown, portal, spacebg, triangle, ticktimer, flashingText, F, VM) {r
 	
 	},
     create: function () {
+		// Add ticktimer to setup events
         this.addSkel(ticktimer);
-        this.addSkel(flashingText);
+		// Play music!
         this.game.audiosprite.play('bgm-moon');
+		// Add space background
         this.addSkel(spacebg);
         this.portal = undefined;
+		
+		// Place player in correct position
         this.player.x = this.game.width/2;
-        this.player.y = this.game.height/3;
+        this.player.y = (this.game.height/3)-300;
+		// Set Background Color just in case
 		this.game.stage.backgroundColor = '#034b59';
 		
+		// timer to spawn a platform on interval
 		this.addTicktimerEvent(100, function (ticks) {
 		    let p = this.spawnPlatform(
 		        'platform-moon-' + (Math.floor(Math.random()*3) + 1),
@@ -45,19 +49,10 @@ function (keyDown, portal, spacebg, triangle, ticktimer, flashingText, F, VM) {r
 		    p.body.velocity.y *= ticks/1200 + 1;
 		    return 0.983;
 		});
-		this.addTicktimerEvent(1300, function () {
-		    this.flashingText('Leaving orbit...');
-		    return false; // don't run this again, only once
-		});
-        this.addTicktimerEvent(1700, function () {
-            this.flashingText('Incoming rectangulo-planetoid field!!');
-            return false;
-        });
+		
         // Use arcade physics
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
-		
-//        this.game.audiosprite.play('spacelava');
-		
+				
 		this.player.y = this.game.height; // Start the player at the bottom of the screen
 		let g = VM.rotate(this.player.body.gravity, 180);
         this.player.body.gravity.x = g.x;
@@ -70,45 +65,45 @@ function (keyDown, portal, spacebg, triangle, ticktimer, flashingText, F, VM) {r
 		this.triangularDude.body.angularVelocity = 6;
 		this.triangularDude.anchor.setTo(0.5, 0.5);
 		
+		// Add solids group
         this.groups.solids = this.add.group();
         this.groups.solids.enableBody = true;
-        this.background = this.groups.solids.create(0,0,'atlas','moon');
-        this.addTicktimerEvent(1, function () {this.background.y -= 0.15;});
-        this.background.anchor.setTo(0,1);
-        this.background.body.immovable=true;
-        this.background.width = this.game.width;
-        this.background.height = -1*(this.game.height/5);
+		
+		// Add moon to solids
+        this.moon = this.groups.solids.create(0,0,'atlas','moon');
+		// Set the moon to scroll
+        this.addTicktimerEvent(1, function () {this.moon.y -= 0.15;});
+        this.moon.anchor.setTo(0,1);
+        this.moon.body.immovable=true;
+        this.moon.width = this.game.width;
+        this.moon.height = -1*(this.game.height/5);
+		
+		// Send appropriate objects to back.
         this.world.sendToBack(this.groups.solids);
         this.world.sendToBack(this.triangularDude);
+		
+		// Spawn initial sets of platforms
         for(let i = -1; i<10; i++)
             this.spawnPlatform('platform-moon-' + (Math.floor(Math.random()*3) + 1),
                 Math.random()*400, i*75);
-
 		
-		// Lava is offscreen at the top just to kill the player.
-		// Should fix.
-        let lava = this.groups.hazards.create(0, 0-50, 'atlas', 'lava');
-        lava.width = 800;
-        lava.height = 50;
+		// Lazy way of making a kill zone
+        let killZone = this.groups.hazards.create(0, -100, 'atlas', 'moon');
+        killZone.width = this.game.width;
+        killZone.height = 50;
+
+		// Add event for portal to spawn
         this.addTicktimerEvent(2500, function () {
 		    if (!this.portal) this.addSkel(portal);
 		    this.portal.x = Math.random()*this.game.width;
 		    this.portal.y = this.game.height;
 		    this.portal.body.velocity.y = -120;
         });
-        this.player.y -= 300;
     },
     update: function () {
-
-		//this.groups.solids.forEach(function(platform){platform.body.y += 1;});
-		// Using velocity for this to fix player bounce glitch
-
         // Gooferino the triangle duderino
         this.triangularDude.body.angularVelocity += 0.2;
 		this.entityWrap(this.player); // wrapping on player
 		this.entityWrap(this.triangularDude); // wrapping on triangularDude
-    },
-    render: function () {
-//        this.game.debug.body(this.triangularDude);
     }
 };});
