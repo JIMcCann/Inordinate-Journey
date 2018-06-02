313cfd55628127e2b16bb2a3848f755e0c9998b3
Jaime McCann <bimccann@ucsc.edu>
2018-06-01 18:51:01 -0700

    Integrated Bryan's snow level. Updated atlas with Concio's work

--- js/main.js
+++ js/main.js
@@ -10,15 +10,17 @@ requirejs(['game/states/functionCaller',
             'game/states/skeletons/space_level',
             'game/states/skeletons/side_to_side',
             'game/states/skeletons/planet_level',
+            'game/states/skeletons/rain_level',
             'game/LevelOrder'],
 function (functionCaller, setup, titleScreen,
         tutorial, playertest, spaceLevel, sideToSide, planetLevel,
+        rainLevel,
         LevelOrder) {
     /*  Runs the preloader and then transitions to the state skeleton compositor.
         The SSC will then make a state out of:
         * the groups skeleton (which makes sure the state has a 'groups' property)
         * the player skeleton (which adds the player character)
         * the playertest skeleton (which adds the silly little test room) */
-    LevelOrder.order = [tutorial, playertest, spaceLevel, sideToSide, planetLevel];
+    LevelOrder.order = [tutorial, rainLevel, playertest, spaceLevel, sideToSide, planetLevel];
     setup('functionCaller', titleScreen);
 });
