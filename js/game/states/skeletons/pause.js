72a790f0deb3f76587707272213269e066e504ac
Jaime McCann <bimccann@ucsc.edu>
2018-06-08 17:40:34 -0700

    Added quit and reset to pause menu

--- js/game/states/skeletons/pause.js
+++ js/game/states/skeletons/pause.js
@@ -1,10 +1,25 @@
 /*	game/states/skeletons/pause
 	Adds Esc as a pause button. */
-define(['game/game'], function (game) {
+define(['game/game', 'game/states/fixed/titleScreen', 'game/states/fadeOut', 'game/states/functionCaller'],
+function (game, titleScreen, fadeOut) {
 	let keydownHandler = function (e) {
-		if (e.keyCode == 27) {
-			if (game.paused) game.paused = false;
-			else game.pauseQueued = true;
+		switch (e.keyCode) {
+			case 27: // escape
+				if (game.paused) game.paused = false;
+				else game.pauseQueued = true;
+				break;
+			case 82: case 114: // r
+				if (game.paused) {
+					game.paused = false;
+					game.resetQueued = true;
+				}
+				break;
+			case 81: case 113: // q
+				if (game.paused) {
+					game.paused = false;
+					game.quitQueued = true;
+				}
+				break;
 		}
 	};
 	if (document.addEventListener) {
@@ -18,14 +33,32 @@ define(['game/game'], function (game) {
 				this.pauseText.destroy();
 				this.pauseText = null;
 			}
+			if (this.pauseExplanation) {
+				this.pauseExplanation.destroy();
+				this.pauseExplanation = null;
+			}
 			if (game.pauseQueued) {
+				game.pauseQueued = false;
 				this.pauseText = this.add.text(0, 0, '(Paused)', {
 					fontSize: 24, fill: '#ff0000'
 				});
+				this.pauseExplanation = this.add.text(0, 0, 'Esc: Resume\nR: Reset Level\n' +
+						'Q: Save and Quit to Title', {
+					fontSize: 16, fill: '#ff0000'
+				});
 				this.pauseText.x = (this.game.width - this.pauseText.width)/2;
-				this.pauseText.y = (this.game.height - this.pauseText.height)/2;
-				game.pauseQueued = false;
+				this.pauseText.y = (this.game.height - this.pauseText.height)/2 - 64;
+				this.pauseExplanation.x = (this.game.width - this.pauseExplanation.width)/2;
+				this.pauseExplanation.y = (this.game.height - this.pauseExplanation.height)/2 + 64;
 				game.paused = true;
+			}
+			if (game.resetQueued) {
+				game.resetQueued = false;
+				this.game.levelOrder.restartLevel();
+			}
+			if (game.quitQueued) {
+				game.quitQueued = false;
+				fadeOut('functionCaller', titleScreen);
 			}
 		}
 	};
